20a44d3f03ddd23356d27fc76b8c04b3b83fb7d5
Cosmin Alexandru <114618755+getcosmin@users.noreply.github.com>
2023-01-05 17:06:38 +0200

    Added new scrolling slider feature.

--- src/sections/DisplayHeroSlider.js
+++ src/sections/DisplayHeroSlider.js
@@ -15,16 +15,6 @@ export default function DisplayHeroSlider(movie) {
         }
         const heroSlider = document.querySelector('.hero-image-slider')
 
-    
-        function throttle(fn, wait) {
-            let time = Date.now();
-            return function() {
-              if ((time + wait - Date.now()) < 0) {
-                fn();
-                time = Date.now();
-              }
-            }
-          }
         const displayProgress = document.querySelectorAll('.slider-circle')
         displayProgress[CARD.position - 1].classList.add('circle-active')
 
